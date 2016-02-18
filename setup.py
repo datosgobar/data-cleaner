3de7b8b612a2cf025f1ceb467b76e0a9c293364a
Agustín Benassi <agusbenassi@gmail.com>
2016-02-18 13:03:52 -0300

    Agrego requirements.txt al manifiesto.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='data-cleaner',
-    version='0.1.1',
+    version='0.1.2',
     description="Paquete para limpieza de datos, según estándares de la SSIPyGA - Gobierno Abierto Argentina",
     long_description=readme + '\n\n' + history,
     author="Gobierno Abierto Argentina",

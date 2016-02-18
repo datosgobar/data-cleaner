3a8091a95a0ff206a66ce9ee1303eb33f716de11
Pablo Lorenzatto <enko0341@gmail.com>
2016-02-18 17:15:36 -0300

    Added implementation with a demo

--- data_cleaner/fingerprint_keyer.py
+++ data_cleaner/fingerprint_keyer.py
@@ -1,85 +1,74 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-Este módulo fue tomado de:
-    https://github.com/tweirick/okstate_bioinformatics_command_line_programs/blob/master/misc_programs/FingerprintKeyer.py
-
-10/23/2012 Tyler Weirick
-An implementation of Google-Refine's FingerprintKeyer algorithm in Python
-written mostly because just about every site with a library I could use is down
-today.
-"""
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import with_statement
-
 import string
-# For the translate make a dictionary and do replaces based on the difference
-# of chars
+import unicodedata
+
+#For the translate make a dictionary and do replaces based on the difference 
+#of chars
 #=============================================================================
-tranlate_dict = {'\\u00C0': 'a', '\\u00C1': 'a', '\\u00C2': 'a', '\\u00C3': 'a',
-                 '\\u00C4': 'a', '\\u00C5': 'a', '\\u00E0': 'a', '\\u00E1': 'a', '\\u00E2': 'a', '\\u00E3': 'a',
-                 '\\u00E4': 'a', '\\u00E5': 'a', '\\u0100': 'a', '\\u0101': 'a', '\\u0102': 'a', '\\u0103': 'a',
-                 '\\u0104': 'a', '\\u0105': 'a',
-                 '\\u00C7': 'c', '\\u00E7': 'c', '\\u0106': 'c', '\\u0107': 'c', '\\u0108': 'c', '\\u0109': 'c',
-                 '\\u010A': 'c', '\\u010B': 'c', '\\u010C': 'c', '\\u010D': 'c',
-                 '\\u00D0': 'd', '\\u00F0': 'd', '\\u010E': 'd', '\\u010F': 'd', '\\u0110': 'd', '\\u0111': 'd',
-                 '\\u00C8': 'e', '\\u00C9': 'e', '\\u00CA': 'e', '\\u00CB': 'e', '\\u00E8': 'e', '\\u00E9': 'e',
-                 '\\u00EA': 'e', '\\u00EB': 'e', '\\u0112': 'e', '\\u0113': 'e', '\\u0114': 'e', '\\u0115': 'e',
-                 '\\u0116': 'e', '\\u0117': 'e', '\\u0118': 'e', '\\u0119': 'e', '\\u011A': 'e', '\\u011B': 'e',
-                 '\\u011C': 'g', '\\u011D': 'g', '\\u011E': 'g', '\\u011F': 'g', '\\u0120': 'g', '\\u0121': 'g',
-                 '\\u0122': 'g', '\\u0123': 'g',
-                 '\\u0124': 'h', '\\u0125': 'h', '\\u0126': 'h', '\\u0127': 'h',
-                 '\\u00CC': 'i', '\\u00CD': 'i', '\\u00CE': 'i', '\\u00CF': 'i', '\\u00EC': 'i', '\\u00ED': 'i',
-                 '\\u00EE': 'i', '\\u00EF': 'i', '\\u0128': 'i', '\\u0129': 'i', '\\u012A': 'i', '\\u012B': 'i',
-                 '\\u012C': 'i', '\\u012D': 'i', '\\u012E': 'i', '\\u012F': 'i', '\\u0130': 'i', '\\u0131': 'i',
-                 '\\u0134': 'j', '\\u0135': 'j',
-                 '\\u0136': 'k', '\\u0137': 'k', '\\u0138': 'k',
-                 '\\u0139': 'l', '\\u013A': 'l', '\\u013B': 'l', '\\u013C': 'l', '\\u013D': 'l', '\\u013E': 'l',
-                 '\\u013F': 'l', '\\u0140': 'l', '\\u0141': 'l', '\\u0142': 'l',
-                 '\\u00D1': 'n', '\\u00F1': 'n', '\\u0143': 'n', '\\u0144': 'n', '\\u0145': 'n', '\\u0146': 'n',
-                 '\\u0147': 'n', '\\u0148': 'n', '\\u0149': 'n', '\\u014A': 'n', '\\u014B': 'n',
-                 '\\u00D2': 'o', '\\u00D3': 'o', '\\u00D4': 'o', '\\u00D5': 'o', '\\u00D6': 'o', '\\u00D8': 'o',
-                 '\\u00F2': 'o', '\\u00F3': 'o', '\\u00F4': 'o', '\\u00F5': 'o', '\\u00F6': 'o', '\\u00F8': 'o',
-                 '\\u014C': 'o', '\\u014D': 'o', '\\u014E': 'o', '\\u014F': 'o', '\\u0150': 'o', '\\u0151': 'o',
-                 '\\u0154': 'r', '\\u0155': 'r', '\\u0156': 'r', '\\u0157': 'r', '\\u0158': 'r', '\\u0159': 'r',
-                 '\\u015A': 's', '\\u015B': 's', '\\u015C': 's', '\\u015D': 's', '\\u015E': 's', '\\u015F': 's',
-                 '\\u0160': 's', '\\u0161': 's', '\\u017F': 's',
-                 '\\u0162': 't', '\\u0163': 't', '\\u0164': 't', '\\u0165': 't', '\\u0166': 't', '\\u0167': 't',
-                 '\\u00D9': 'u', '\\u00DA': 'u', '\\u00DB': 'u', '\\u00DC': 'u', '\\u00F9': 'u', '\\u00FA': 'u',
-                 '\\u00FB': 'u', '\\u00FC': 'u', '\\u0168': 'u', '\\u0169': 'u', '\\u016A': 'u', '\\u016B': 'u',
-                 '\\u016C': 'u', '\\u016D': 'u', '\\u016E': 'u', '\\u016F': 'u', '\\u0170': 'u', '\\u0171': 'u',
-                 '\\u0172': 'u', '\\u0173': 'u',
-                 '\\u0174': 'w', '\\u0175': 'w',
-                 '\\u00DD': 'y', '\\u00FD': 'y', '\\u00FF': 'y', '\\u0176': 'y', '\\u0177': 'y', '\\u0178': 'y',
-                 '\\u0179': 'z', '\\u017A': 'z', '\\u017B': 'z', '\\u017C': 'z', '\\u017D': 'z', '\\u017E': 'z'}
 
+def strip_accents(s):
+    if type(s) == str :
+        s = unicode(s, "utf-8")
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
 
-def finger_print_keyer(key_string):
-    assert type(key_string) == str, "ERROR: key must be a string."
-
-    # remove leading and trailing whitespace
+def FingerprintKeyer(key_string):
+    #assert type(key_string) == str,"ERROR: key must be a string."
+    #assert type(key_string) == str,"ERROR: key must be a string."
+    if type(key_string) != str :
+        key_string = key_string.encode('utf8')
+    
+    #remove leading and trailing whitespace
     key_string = key_string.strip().lower()
-
-    # change all characters to their lower case representation
+    
+    #change all characters to their lower case representation
     key_string = key_string.lower()
-
-    # remove all punctuation and control characters
+    
+    #remove all punctuation and control characters
     for punct in (set(key_string) & set(string.punctuation)):
-        key_string = key_string.replace(punct, "")
-
-    # split the string into whitespace-separated tokens
+        key_string = key_string.replace(punct,"")
+    
+    key_string = key_string.replace("\t", " ")
+    
+    #split the string into whitespace-separated tokens
     split_key = key_string.split()
-
-    # sort the tokens and remove duplicates
+    
+    #sort the tokens and remove duplicates
     unique_sorted_split_key = sorted(set(split_key))
-
-    # join the tokens back together
-    finger_printed_key = "".join(unique_sorted_split_key)
-
-    # normalize extended western characters to their ASCII
-    # representation (for example "gödel" → "godel")
-    # Leaving this out for now as it should not matter for protein names.
-    # translate()
+    
+    #join the tokens back together
+    finger_printed_key = " ".join(unique_sorted_split_key)
+    
+    #normalize extended western characters to their ASCII 
+    #representation (for example "gödel" → "godel")
+    finger_printed_key = strip_accents(finger_printed_key)
     return finger_printed_key
+
+
+def GroupFingerprintStrings(inp) :
+    """
+    Retorna un diccionario donde las keys son los fingerprints y los valores las strings originales
+    Tambien retorna un conteo de las strings originales
+    """
+    res = {}
+    counts = {}
+    for (k,s) in zip(map(FingerprintKeyer, inp), inp) :
+        res[k] = res.get(k, []) + [s]
+        counts[s] = counts.get(s, 0) + 1
+    return res, counts
+
+def GetBestReplacements(d, counts) :
+    """
+    Itera por cada cluster para determinar la mejor string para reemplazar las strings originales
+    De momento solo utiliza un conteo simple pero podria agregarse algun criterio basado en la capitalizacion de las palabras
+    """
+    res = {}
+    for (k,v) in d.items() :
+        res[k] = max(v, key=lambda s: counts[s])
+    return res
+    
+def ReplaceByKey(d, l) :
+    return [d.get(FingerprintKeyer(s), s) for s in l]
+
---
+++ data_cleaner/fingerprint_keyer_demo.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+from pprint import pprint
+
+from fingerprint_keyer import FingerprintKeyer, GroupFingerprintStrings, GetBestReplacements, ReplaceByKey
+
+testStrings = [
+    u" - juan     peRes",
+    u"Juan ; Perés",
+    u"Juan -- Peres",
+    u"Juan Per\tes",
+    u"juán Peres",
+    u"Juan Peres",
+    u"Juan Peres",
+    u"   Juan\t \tPeres",
+]
+
+def demo1() :
+    print "="*80
+    print "Demo 1"
+    print "="*80
+
+    testStrings = [
+            ("schön","schon"),  
+            #("Ære Øre Åre", "are aere ore"),
+            #("Straße","strasse"),
+            ("\tABC \t DEF ","abc def"), # test leading and trailing whitespace
+            ("bbb\taaa","aaa bbb"),
+            ("müller","muller"),
+            #("müller","mueller"), # another possible interpretation
+            #("ﬁﬂĳ","fiflij"),
+            #("ﭏ","אל"),
+            #("œ ӕ","ae oe"),
+            ("",""),
+    ]
+
+    for (inp, out_exp) in testStrings :
+        r = FingerprintKeyer(inp)
+        print "inp=%s" % inp
+        print "out_exp=%s" % out_exp
+        print "r=%s" % r
+        print "FingerprintKeyer(%s)" % inp
+        print "result=%s" % (r==out_exp)
+        print ""
+
+
+def demo2():
+    print "="*80
+    print "Demo 2"
+    print "="*80
+    
+    print "Strings originales"
+    for s in testStrings :
+        print s
+    print ""
+
+    #df = pd.read_csv("audiencias-raw.csv")
+    #testStrings = df.lugar
+
+    clusters, counts = GroupFingerprintStrings(testStrings)
+    print "Clusters encontrados"
+    pprint(clusters)
+    print ""
+
+    print "Conteos de strings"
+    pprint(counts)
+    print ""
+
+    # Para cada cluster tomo la string raw que resulte mas apta
+    d = GetBestReplacements(clusters, counts)
+    print "Strings de replazo para cada key"
+    pprint(d)
+    print ""
+
+    # Reemplazo las keys que matchean el fingerprint una version
+    print "Output:"
+    for s in ReplaceByKey(d, testStrings) :
+        print s
+    print ""
+
+if __name__ == "__main__" :
+    demo1()
+    demo2()

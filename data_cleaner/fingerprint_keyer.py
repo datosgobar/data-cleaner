40ca3a0d1f7574bb712cda7ca4f31ec92adfd67c
Agustín Benassi <agusbenassi@gmail.com>
2016-02-21 16:53:56 -0300

    Completo docstrings y tests de fingerprint.

--- data_cleaner/fingerprint_keyer.py
+++ data_cleaner/fingerprint_keyer.py
@@ -15,6 +15,9 @@ def fingerprint_keyer(key_string):
 
     Args:
         key_string (str): String para convertir en fingerprint key.
+
+    Returns:
+        str: Fingerprint correspondiente al input.
     """
     # enforece string type
     if type(key_string) != str:
@@ -45,37 +48,46 @@ def fingerprint_keyer(key_string):
     return finger_printed_key
 
 
-def group_fingerprint_strings(inp):
+def group_fingerprint_strings(raw_strs):
     """Clusteriza un conjunto de strings, según sus fingerprints.
 
     Args:
-        inp (list): Lista de strings.
+        raw_strs (list): Lista de strings sin procesar.
 
     Returns:
         (dict, dict): En el primer dict las keys son los fingerprints y los
             valores las strings originales. En el segundo las keys son las
-            strings sin normalizar y los valores el conteo de las strings
-            originales.
+            strings sin normalizar y los valores el conteo de la cantidad de
+            veces que aparecen.
     """
     res = {}
     counts = {}
-    for (key, raw_str) in zip(map(fingerprint_keyer, inp), inp):
+    for (key, raw_str) in zip(map(fingerprint_keyer, raw_strs), raw_strs):
         res[key] = res.get(key, []) + [raw_str]
         counts[raw_str] = counts.get(raw_str, 0) + 1
     return res, counts
 
 
-def get_best_replacements(d, counts):
-    """
+def get_best_replacements(clusters, counts):
+    """Selecciona los strings más utilizados por cluster.
+
     Itera por cada cluster para determinar la mejor string para reemplazar las
     strings originales. De momento solo utiliza un conteo simple pero podria
     agregarse algun criterio basado en la capitalizacion de las palabras
+
+    Args:
+        clusters (dict): {fingerprint: [raw_string_1, raw_string_2]}
+        counts (dict): {raw_string: cant_veces_utilizada}
+
+    Returns:
+        dict: {fingerprint: string_mas_usada_para_esa_fingerprint}
     """
     res = {}
-    for (k, v) in d.items():
-        res[k] = max(v, key=lambda s: counts[s])
+    for (fingerprint, key_stings) in clusters.items():
+        res[fingerprint] = max(key_stings, key=lambda s: counts[s])
     return res
 
 
-def replace_by_key(d, l):
-    return [d.get(fingerprint_keyer(s), s) for s in l]
+def replace_by_key(replacements, raw_strs):
+    """Reemplaza strings por sus mejores equivalentes."""
+    return [replacements.get(fingerprint_keyer(s), s) for s in raw_strs]

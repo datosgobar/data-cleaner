be03d0455eccf579e7eaa844304c2fc8f99fbdf4
Marisol Cardozo <cardozo.marisolp@gmail.com>
2018-08-03 16:21:52 -0300

    Renombra módulo que contiene el cliente de Georef API.

--- data_cleaner/georef_api.py
+++ data_cleaner/georef_api.py
@@ -3,10 +3,22 @@
 
 import urllib
 import json
+import os
 
 
-# TODO: tomar de una variable de entorno
-API_URL = 'http://localhost:5000/api/'
+API_URL = os.environ['API_URL']
+ID = 'id'
+NAME = 'nombre'
+PROV = 'provincia'
+PROV_ID = 'provincia_id'
+PROV_NAM = 'provincia_nombre'
+DEPT = 'departamento'
+DEPT_ID = 'departamento_id'
+DEPT_NAM = 'departamento_nombre'
+MUN = 'municipio'
+MUN_ID = 'municipio_id'
+MUN_NAM = 'municipio_nombre'
+LOCALITY = 'localidad'
 
 
 class GeorefWrapper:
@@ -15,7 +27,7 @@ class GeorefWrapper:
     def __init__(self):
         self.url = API_URL
 
-    def search_state(self, search_name):
+    def search_province(self, search_name):
         entity = 'provincias'
         return self._get_response(entity, search_name)
 
@@ -31,12 +43,15 @@ class GeorefWrapper:
         entity = 'localidades'
         return self._get_response(entity, search_name)
 
-    def _get_response(self, entity, search_name):
+    def _get_response(self, entity, search_name, first_element=True):
         resource = self.url + entity + '?'
-        resource += urllib.urlencode({'nombre': search_name})
+        resource += urllib.urlencode({'nombre': search_name, 'max': 1})
         results = json.loads(urllib.urlopen(resource).read())
-        return self._get_first_element(results, entity)
+        if results[entity] and first_element:
+            return self._get_first_element(results, entity)
+        return results[entity]
 
     @staticmethod
     def _get_first_element(results, entity):
-        return results[entity][0]['nombre']
+        if results[entity]:
+            return results[entity][0]

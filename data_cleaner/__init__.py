414d7fa09bf74c23dbf948b8c1fcc32715067dcb
Ignacio Heredia <ignacio.nh@gmail.com>
2019-02-06 19:41:49 -0300

    Fix relative imports

--- data_cleaner/__init__.py
+++ data_cleaner/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
-from data_cleaner import DataCleaner
-from georef_api import GeorefWrapper
+from .data_cleaner import DataCleaner
+from .georef_api import GeorefWrapper
 
 __author__ = """Datos Argentina"""
 __email__ = 'datos@modernizacion.gob.ar'
